bc6587f44b9681180ef8b8f6ff50660b75facfc8
= <webdev.jameslemire@gmail.com>
2017-01-06 15:05:44 -0700

    JL master - :construction:

--- karma.conf.js
+++ karma.conf.js
@@ -9,13 +9,17 @@ module.exports = function(config) {
 
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['jasmine'],
+		frameworks: [ 'browserify', 'jasmine' ],
 
 		// list of files / patterns to load in the browser
 		files: [
 			'node_modules/quixote/dist/quixote.js',
-			'test/*.js'
+			'test/**/*.js'
 		],
+
+		preprocessors: {
+			'test/**/*.js': [ 'browserify' ]
+		},
 
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
@@ -37,7 +41,7 @@ module.exports = function(config) {
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['PhantomJS'],
+		browsers: ['Chrome'],
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
