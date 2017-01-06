2df12ec7daa46ee3c2e7588d78d4afddb0fc477b
= <webdev.jameslemire@gmail.com>
2017-01-06 14:07:46 -0700

    JL master - Unit test progress.

---
+++ test/css.js
@@ -0,0 +1,27 @@
+describe("DevMtn-CSS-Store", function() {
+	var frame, containerHeader;
+
+	before(function(done) {
+		frame = quixote.createFrame({
+			src: "/"
+		}, done);
+	});
+
+	after(function() {
+		frame.remove();
+	});
+
+	beforeEach(function(done) {
+		frame.reload(done);
+	});
+
+	beforeEach(function() {
+		containerHeader = frame.get("#container-header");
+	});
+
+	it('Header container is 100% in width', function() {
+		containerHeader.assert({
+			width: '100%'
+		}, "The header container should have a width of 100%");
+	});
+});
